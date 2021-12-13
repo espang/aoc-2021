930ae8da1b253e31288e0c32515d41c1ba41d463
Eike Spang <eike.spang@gmail.com>
2021-12-13 19:24:52 +0100

    day9

---
+++ src/aoc/day9.clj
@@ -0,0 +1,53 @@
+(ns aoc.day9
+  (:require
+   [aoc.helpers :refer [read-input-lines]]
+   [clojure.string :as str]))
+
+(defn parse-line [s]
+  (mapv (comp parse-long str) (seq s)))
+
+(defn value-at [[row col] matrix]
+  "Returns the value at the cell or 10,
+   a value that is larger than any expected value"
+  (nth (nth matrix row []) col 10))
+
+(defn low-point [[row col] matrix]
+  (let [v (value-at [row col] matrix)
+        neighbours (map (fn [[drow dcol]] [(+ row drow) (+ col dcol)])
+                        [[-1 0] [1 0] [0 -1] [0 1]])]
+    (every? #(< v (value-at % matrix)) neighbours)))
+
+(defn part1 [matrix]
+  (let [max-row (count matrix)
+        max-col (count (first matrix))]
+    (->> (for [row (range max-row)
+               col (range max-col)
+               :when (low-point [row col] matrix)]
+           (value-at [row col] matrix))
+         (map inc)
+         (reduce +))))
+
+(comment
+  (part1 (vec (read-input-lines 9 parse-line true)))
+  (part1 (vec (read-input-lines 9 parse-line))))
+
+
+(defn basin-size [[row col] matrix]
+  (loop [q (conj (clojure.lang.PersistentQueue/EMPTY) [row col])
+         seen #{}
+         count 0]
+    (println (seq q) seen count)
+    (if (empty? q)
+      count
+      (let [e (peek q)
+            neighbours (->> [[-1 0] [1 0] [0 -1] [0 1]]
+                            (map (fn [[drow dcol]] [(+ row drow) (+ col dcol)]))
+                            (filter #(> 9 (value-at % matrix))))]
+        (if (contains? seen e)
+          (recur (pop q) seen count)
+          (recur (apply conj (pop q) neighbours)
+                 (conj seen e)
+                 (inc count)))))))
+
+(comment 
+  (basin-size [0 9] (vec (read-input-lines 9 parse-line true))))
